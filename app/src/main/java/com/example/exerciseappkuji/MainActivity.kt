6e7bddb4bea40810ba8422c10160b01a4b9daff5
Richard Li <s-liric@bsd405.org>
2021-10-04 21:12:49 -0700

    commit

--- app/src/main/java/com/example/exerciseappkuji/MainActivity.kt
+++ app/src/main/java/com/example/exerciseappkuji/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.exerciseappkuji
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun createNewJournal(view: View)
+    {
+        val TextView  = findViewById<TextView>(R.id.CreateNewJournal)
+        var message = TextView.text.toString()
+        intent = Intent(this, DisplayMessageActivity::class.java).apply
+        {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+    private fun putExtra(extraMessage: String, message: String): Any {
+
+    }
+
+    fun addToExistingJournal(view: View)
+    {
+
+    }
+
 }
